1bddaed69045817ce900fcb13e5d1dbc274e80e3
Carl Meyer <carl@dirtcircle.com>
2009-03-26 17:06:15 -0500

    bump version to 0.2.0pre
    --HG--
    extra : convert_revision : carl%40dirtcircle.com-20090326220615-ihe3nwmbiwwaveiy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-form-utils',
-    version='0.1.0',
+    version='0.2.0pre',
     description='Form utilities for Django',
     author='Carl Meyer',
     author_email='carl@dirtcircle.com',

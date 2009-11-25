ed4e25b56d2e7a72f5917a83a70c1abaa85f01e2
Carl Meyer <carl@dirtcircle.com>
2009-11-25 00:25:50 -0500

    mv ClearableFileField to fields.py

--- form_utils/widgets.py
+++ form_utils/widgets.py
@@ -1,7 +1,7 @@
 """
 widgets for django-form-utils
 
-Time-stamp: <2009-09-02 23:14:21 carljm widgets.py>
+Time-stamp: <2009-11-24 23:24:06 carljm widgets.py>
 
 parts of this code taken from http://www.djangosnippets.org/snippets/934/
  - thanks baumer1122
@@ -71,48 +71,3 @@ class ClearableFileInput(forms.MultiWidget):
             return self.template % {'input': rendered_widgets[0],
                                     'checkbox': rendered_widgets[1]}
         return rendered_widgets[0]
-
-class FakeEmptyFieldFile(object):
-    """
-    A fake FieldFile that will convice a FileField model field to
-    actually replace an existing file name with an empty string.
-    
-    FileFild.save_form_data only overwrites its instance data if the
-    incoming form data evaluates to True in a boolean context (because
-    an empty file input is assumed to mean "no change"). We want to be
-    able to clear it without requiring the use of a model FileField
-    subclass (keeping things at the form level only). In order to do
-    this we need our form field to return a value that evaluates to
-    True in a boolean context, but to the empty string when coerced to
-    unicode. This object fulfills that requirement.
-
-    It also needs the _committed attribute to satisfy the test in
-    FileField.pre_save.
-
-    This is, of course, hacky and fragile, and depends on internal
-    knowledge of the FileField and FieldFile classes. But it will
-    serve until Django FileFields acquire a native ability to be
-    cleared (ticket 7048).
-
-    """
-    def __unicode__(self):
-        return u''
-    _committed = True
-
-class ClearableFileField(forms.MultiValueField):
-    default_file_field_class = forms.FileField
-    widget = ClearableFileInput
-    
-    def __init__(self, file_field=None, *args, **kwargs):
-        file_field = file_field or self.default_file_field_class(*args,
-                                                                  **kwargs)
-        fields = (file_field, forms.BooleanField(required=False))
-        kwargs['required'] = file_field.required
-        kwargs['widget'] = self.widget(file_widget=file_field.widget)
-        super(ClearableFileField, self).__init__(fields, *args, **kwargs)
-
-    def compress(self, data_list):
-        if data_list[1] and not data_list[0]:
-            return FakeEmptyFieldFile()
-        return data_list[0]
-

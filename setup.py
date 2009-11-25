540f31a2d0a2ee4ab2c13b3b9421ccb2ff9fd8aa
Carl Meyer <carl@dirtcircle.com>
2009-11-25 04:33:54 -0500

    bump version for release

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 from setuptools import setup
+import subprocess
+import os.path
 
+try:
+    # don't get confused if our sdist is unzipped in a subdir of some 
+    # other hg repo
+    if os.path.isdir('.hg'):
+        p = subprocess.Popen(['hg', 'parents', r'--template={rev}\n'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if not p.returncode:
+            fh = open('HGREV', 'w')
+            fh.write(p.communicate()[0].splitlines()[0])
+            fh.close()
+except (OSError, IndexError):
+    pass
+    
+try:
+    hgrev = open('HGREV').read()
+except IOError:
+    hgrev = ''
+    
 long_description = open('README.txt').read() + open('CHANGES.txt').read()
 
 setup(
     name='django-form-utils',
-    version='0.1.5.post',
+    version='0.1.6',
     description='Form utilities for Django',
     long_description=long_description,
     author='Carl Meyer',

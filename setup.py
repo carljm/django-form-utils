48f1acf437088e2d19ea87c3995a19de22e0f303
Carl Meyer <carl@dirtcircle.com>
2010-03-16 17:26:52 -0400

    bump version for release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ long_description = open('README.rst').read() + open('CHANGES.rst').read()
 
 setup(
     name='django-form-utils',
-    version='0.1.7.post%s' % hgrev,
+    version='0.1.8',
     description='Form utilities for Django',
     long_description=long_description,
     author='Carl Meyer',

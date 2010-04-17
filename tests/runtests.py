0baa40766c625d0107db6d07e1d3075d1f7938c8
Carl Meyer <carl@dirtcircle.com>
2010-04-16 22:58:40 -0400

    Django 1.2 compatible test runner

--- tests/runtests.py
+++ tests/runtests.py
@@ -7,12 +7,10 @@ sys.path.insert(0, parent)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
 
-from django.test.utils import get_runner
-from django.conf import settings
+from django.test.simple import run_tests
 
 def runtests():
-    test_runner = get_runner(settings)
-    failures = test_runner(['tests'], verbosity=1, interactive=True)
+    failures = run_tests(['tests'], verbosity=1, interactive=True)
     sys.exit(failures)
 
 if __name__ == '__main__':

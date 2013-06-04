0d9c5aa366118457001a5087233e8479e73ddd76
Carl Meyer <carl@oddbird.net>
2013-06-04 10:06:44 -0600

    Bump version for dev.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ long_description = open('README.rst').read() + open('CHANGES.rst').read()
 
 setup(
     name='django-form-utils',
-    version='0.3',
+    version='0.3.0.post%s' % hgrev,
     description='Form utilities for Django',
     long_description=long_description,
     author='Carl Meyer',
